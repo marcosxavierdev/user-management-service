0467a95671ba943692654bc7a6a5ad24a4994cbe
marcosxavierdev <sis.marcosxavier@gmail.com>
2024-10-24 19:36:18 -0300

    add endpoint health

---
+++ src/main/java/com/consulting/user_management_service/controller/UserManagementService.java
@@ -0,0 +1,18 @@
+package com.consulting.user_management_service.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/user-management-service")
+public class UserManagementService {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealth() {
+        return ResponseEntity.ok("user-management-service UP!");
+    }
+}
